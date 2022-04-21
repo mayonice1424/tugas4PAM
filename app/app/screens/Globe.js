362eb583a8935b85f2f95568e7add3dbc1a981bf
mayonice1424 <friyandrailham14@gmail.com>
2022-04-21 23:03:49 +0700

    Finishing

--- app/app/screens/Globe.js
+++ app/app/screens/Globe.js
@@ -2,28 +2,23 @@ import * as React from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Dimensions } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation={true}
-      />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
     ...StyleSheet.absoluteFillObject,
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+    ></MapView>
+  </View>
+);
